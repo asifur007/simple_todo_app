d247d1145073012d08d32b4b8f7c037a49359601
asifur007 <asif.sadi00@gmail.com>
2023-05-10 20:57:26 +0600

    version 1.0 completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import '../utils/dialog_box.dart';
+import '../utils/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //Text Controller
+  final _controller = TextEditingController();
+  //list of toDo task
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Exercise", false],
+  ];
+
+  //checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  //save new task
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  //create a new task
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+
+  // delete a task
+  void deleteTask(int index){
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: const Text('ToDo App'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/utils/dialog_box.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // get user input
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Add a new task',
+              ),
+            ),
+            //buttons --> Save & Cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // save  button
+                MyButton(text: "save", onPressed: onSave),
+                const SizedBox(width: 10),
+                // cancel button
+                MyButton(text: "cancel", onPressed: onCancel),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
